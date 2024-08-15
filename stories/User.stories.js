e0f253c3023198ac78cc065b96072b5082395e65
admin <admin@localhost.localdomain>
2024-08-15 16:33:18 +0700

    update

---
+++ stories/User.stories.js
@@ -0,0 +1,29 @@
+import { ArgTypes } from "@storybook/blocks";
+import { createUser } from "./User";
+
+export default {
+  title: "Component/User",
+  tags: ["autodocs"],
+  //   parameters: {
+  //     layout: "centered",
+  //   },
+};
+
+const Template = (args) => {
+  return createUser(args.columns);
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  columns: [
+    "Name",
+    "Read",
+    "Update",
+    "Create",
+    "Delete",
+    "Purge",
+    "Read Notes",
+    "Update Notes",
+    "Select/Unselect All",
+  ],
+};
